6aed46e381b8d311954a5368dd0b666920326748
dwiaditya25 <afifaturahman@gmail.com>
2023-11-25 05:22:18 +0700

    Coba Login maaf kalo masih banyak kurangnya

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://magento.softwaretestingboard.com/",
+    env: {
+      username: "",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 5500,
+    screenshotOnRunFailure: false,
+    chromeWebSecurity: false,
   },
-};
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,6 +12,15 @@
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
+Cypress.Commands.add("login", (email, passw) => {
+  cy.get("#email").clear().type(email);
+  cy.get("#pass").clear().type(passw);
+  cy.get(".action.login.primary").click();
+});
+
+Cypress.Commands.add("verifyContain", (locator, value) => {
+  cy.get(locator).should("contain", value);
+});
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
